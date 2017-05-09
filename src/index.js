c98c13d298196783792c08f0af04d0a2080d4609
Pedro Torrent F <ptorrent@jri.cl>
2017-05-09 16:51:36 -0300

    class component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBBL0tr9sg4LlbjSydcIFDZIy0u-RjhRxU';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = { videos: [] }; 
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => { //Se puede factorizar function(data) = (data) =>
+      this.setState({ videos }); //se factoriza videos: videos = videos Esto porque son el mismo string, caso particular.
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
